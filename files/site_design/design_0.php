abd5e8fca30becdd1d426570e8d5881c2733796f
Andy An <hu7055@naver.com>
2016-02-28 22:01:03 -0500

    new web

---
+++ files/site_design/design_0.php
@@ -0,0 +1,14 @@
+<?php if(!defined("__XE__")) exit();
+$designInfo = new stdClass;
+$designInfo->layout_srl = 88; 
+$designInfo->mlayout_srl = 89;
+$designInfo->module = new stdClass;
+$designInfo->module->page = new stdClass;
+$designInfo->module->page->mskin = 'default';
+$designInfo->module->page->skin = 'default';
+$designInfo->module->board = new stdClass;
+$designInfo->module->board->mskin = 'default';
+$designInfo->module->board->skin = 'xedition';
+$designInfo->module->editor = new stdClass;
+$designInfo->module->editor->mskin = '';
+$designInfo->module->editor->skin = 'ckeditor';
